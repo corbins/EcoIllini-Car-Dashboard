c9a3d81352fb5b1b2066be087baf47cb24595d41
Corbin Souffrant <souffra2@illinois.edu>
2013-03-30 18:48:13 -0500

    Finished with the sketch until I have access to my board.

--- board/ecoillini_sketch/ecoillini_sketch.ino
+++ board/ecoillini_sketch/ecoillini_sketch.ino
@@ -1,7 +1,7 @@
 //Ecoillini Car Dashboard Sketch
 //Test Board -- LED + Switch + potentiometer + Button
 //Developed by: Corbin Souffrant
-//Referenced: http://jeffreysambells.com/2011/05/17/understanding-the-demokit-pde-arduino-sketch
+
 
 #include <Wire.h>
 #include <Servo.h>
@@ -21,14 +21,12 @@
 #define LED1_BLUE 4
 
 //Identify the Accessory
-AndroidAccessory a_device("Ecoillini",
+AndroidAccessory acc("Ecoillini",
                           "Car Dashboard",
                           "Car Dashboard Controller",
                           "0.1",
                           "https://github.com/corbins/EcoIllini-Car-Dashboard",
                           "0102030212345678"); //Serial?  I think I can just throw a random value here
-                          
-//TODO: Initialize the hardware
 
 //Initial board setup.
 void setup();
@@ -49,19 +47,17 @@ void init_hardware() {
   pinMode(LED1_BLUE, OUTPUT);
 }
 
-//Button state variable
+//State variables for hardware
 byte b1;
+int pot1;
 
 void setup() {
-  Serial.begin(115200); //TODO: wtf is this?
+  Serial.begin(115200);
   Serial.print("\r\nStart");
   
   init_hardware();
-  
-  //TODO: I think I have to set up the analog stuff here... probably...
   b1 = digitalRead(BUTTON1);
-  
-  a_device.powerOn();
+  acc.powerOn();
 }
 
 void loop() {
@@ -69,10 +65,11 @@ void loop() {
   byte idle;
   static byte count = 0;
   byte msg[3];
+  byte potmsg[6];
   
   //Look for a connection and go, go, go!
-  if(a_device.isConnected()) {
-    int len = a_device.read(msg, sizeof(msg), 1);
+  if(acc.isConnected()) {
+    int len = acc.read(msg, sizeof(msg), 1);
     int i;
     byte b;
     
@@ -90,18 +87,25 @@ void loop() {
       }
     }
     
-    //Are we sending any data back? 0x1 is output command
+    //Are we sending any data back? 0x1 is output command.
     msg[0] = 0x1;
     
     b = digitalRead(BUTTON1);
     if(b != b1) {
       msg[1] = 0;
       msg[2] = b ? 0 : 1;
-      a_device.write(msg, 3);
+      acc.write(msg, 3);
       b1 = b;
     }
     
-    //TODO: Analog inputs go here, no clue what I need.
+    pot1 = analog_read(POTENTIOMETER1);
+    potmsg[0] = 0x3; //0x3 is analog output command.
+    potmsg[1] = 0x0;
+    potmsg[2] = (byte) (pot1 >> 24);
+    potmsg[3] = (byte) (pot1 >> 16);
+    potmsg[4] = (byte) (pot1 >> 8);
+    potmsg[5] = (byte) (pot1);
+    acc.write(potmsg, 6);
   } else {
     //No connection.  Reset values to prevent bad things from happening.
     analogWrite(LED1_RED, 255);
@@ -109,5 +113,8 @@ void loop() {
     analogWrite(LED1_BLUE, 255);
   }
   
-  delay(10);
+  delay(100);
 }
+
+//Referenced: http://jeffreysambells.com/2011/05/17/understanding-the-demokit-pde-arduino-sketch
+//Referenced: Beginning Android ADK with Arduino by Mario Bohmer
