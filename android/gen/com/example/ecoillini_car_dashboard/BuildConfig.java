2141ba020a961cfb21eb3cc743809f1f49fff216
Corbin Souffrant <souffra2@illinois.edu>
2013-03-30 19:47:44 -0500

    Starting the Android project

---
+++ android/gen/com/example/ecoillini_car_dashboard/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.ecoillini_car_dashboard;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
