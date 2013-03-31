d087ea6a48aca9c0b086bc5de3900965e3e40541
Corbin Souffrant <souffra2@illinois.edu>
2013-03-31 00:55:03 -0500

    Needed this to be a blank project...

--- android/gen/com/example/ecoillini_car_dashboard/R.java
+++ android/gen/com/example/ecoillini_car_dashboard/R.java
@@ -9,33 +9,24 @@ package com.example.ecoillini_car_dashboard;
 
 public final class R {
     public static final class attr {
-        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
-or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
-         */
-        public static final int buttonBarButtonStyle=0x7f010001;
-        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
-or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
-         */
-        public static final int buttonBarStyle=0x7f010000;
-    }
-    public static final class color {
-        public static final int black_overlay=0x7f040000;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int dummy_button=0x7f070002;
-        public static final int fullscreen_content=0x7f070000;
-        public static final int fullscreen_content_controls=0x7f070001;
+        public static final int menu_settings=0x7f080001;
+        public static final int toggleButtonLED=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_fullscreen=0x7f030000;
+        public static final int activity_main=0x7f030000;
+    }
+    public static final class menu {
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
-        public static final int dummy_button=0x7f050002;
-        public static final int dummy_content=0x7f050001;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
     }
     public static final class style {
         /** 
@@ -64,51 +55,8 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
-        public static final int ButtonBar=0x7f060003;
-        public static final int ButtonBarButton=0x7f060002;
-        public static final int FullscreenActionBarStyle=0x7f060005;
-        public static final int FullscreenTheme=0x7f060004;
     }
-    public static final class styleable {
-        /** 
-         Declare custom theme attributes that allow changing which styles are
-         used for button bars depending on the API level.
-         ?android:attr/buttonBarStyle is new as of API 11 so this is
-         necessary to support previous API levels.
-    
-           <p>Includes the following attributes:</p>
-           <table>
-           <colgroup align="left" />
-           <colgroup align="left" />
-           <tr><th>Attribute</th><th>Description</th></tr>
-           <tr><td><code>{@link #ButtonBarContainerTheme_buttonBarButtonStyle com.example.ecoillini_car_dashboard:buttonBarButtonStyle}</code></td><td></td></tr>
-           <tr><td><code>{@link #ButtonBarContainerTheme_buttonBarStyle com.example.ecoillini_car_dashboard:buttonBarStyle}</code></td><td></td></tr>
-           </table>
-           @see #ButtonBarContainerTheme_buttonBarButtonStyle
-           @see #ButtonBarContainerTheme_buttonBarStyle
-         */
-        public static final int[] ButtonBarContainerTheme = {
-            0x7f010000, 0x7f010001
-        };
-        /**
-          <p>This symbol is the offset where the {@link com.example.ecoillini_car_dashboard.R.attr#buttonBarButtonStyle}
-          attribute's value can be found in the {@link #ButtonBarContainerTheme} array.
-
-
-          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
-or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
-          @attr name android:buttonBarButtonStyle
-        */
-        public static final int ButtonBarContainerTheme_buttonBarButtonStyle = 1;
-        /**
-          <p>This symbol is the offset where the {@link com.example.ecoillini_car_dashboard.R.attr#buttonBarStyle}
-          attribute's value can be found in the {@link #ButtonBarContainerTheme} array.
-
-
-          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
-or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
-          @attr name android:buttonBarStyle
-        */
-        public static final int ButtonBarContainerTheme_buttonBarStyle = 0;
-    };
+    public static final class xml {
+        public static final int accessory_filter=0x7f040000;
+    }
 }
